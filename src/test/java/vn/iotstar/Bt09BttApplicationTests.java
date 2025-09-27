c410642cff53965a3e339795052d13e1f8468436
Bui Thanh Tung <buithanhtungaabb@gmail.com>
2025-09-27 07:45:25 +0700

    Mot so file bo sung

---
+++ src/test/java/vn/iotstar/Bt09BttApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt09BttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
